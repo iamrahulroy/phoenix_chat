8f48582c58713fcf66597ce14e902caeb11981ed
Rahul Roy <techniciablog@gmail.com>
2016-02-04 00:08:41 +0530

    Add back app.js code

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,3 +19,29 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 import socket from "./socket"
+
+socket.connect();
+
+// Now that you are connected, you can join channels with a topic:
+var channel = socket.channel("rooms:lobby", {});
+var chatInput = $("#chat-input");
+var messagesContainer = $("#messages");
+
+chatInput.on("keypress", function (event) {
+  if (event.keyCode === 13) {
+    channel.push("new_msg", { body: chatInput.val() });
+    chatInput.val("");
+  }
+});
+
+channel.on("new_msg", function (payload) {
+  messagesContainer.append("<br/>[" + Date() + "] " + payload.body);
+});
+
+channel.join().receive("ok", function (resp) {
+  console.log("Joined successfully", resp);
+}).receive("error", function (resp) {
+  console.log("Unable to join", resp);
+});
+
+export default socket
