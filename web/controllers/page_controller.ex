07a66789c1a97e198f1e91a9157bb0244dace771
Rahul Roy <techniciablog@gmail.com>
2016-02-03 22:35:48 +0530

    Add username to chat message

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,11 @@ defmodule PhoenixChat.PageController do
   use PhoenixChat.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    user = current_user(conn)
+    render conn, "index.html", test: user
+  end
+
+  defp current_user(conn) do
+    Plug.Conn.get_session(conn, :current_user)
   end
 end
